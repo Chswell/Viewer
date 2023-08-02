f1ec9ff77e8280bcc866fe2b426b6376583183a7
Nikita Streltsov <nikitastreltsov324@gmail.com>
2023-08-03 02:35:18 +0700

    Добавлено изображение и его заголовок на страницу с дополнительной информацией и сделана возможность увеличения фотографии

--- src/screens/FullPin.jsx
+++ src/screens/FullPin.jsx
@@ -8,11 +8,21 @@ import {
   Text,
   View,
   FlatList,
+  Modal,
+  TouchableOpacity,
+  Button,
+  Alert,
 } from 'react-native';
 import axios from 'axios';
 import Loading from '../components/Loading';
+import {Dimensions} from 'react-native';
+import Pinchable from 'react-native-pinchable';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height / 1.5;
 
 function FullPin({route}) {
+  const [modalVisible, setModalVisible] = React.useState(false);
   const [isLoading, setIsLoading] = React.useState(true);
   const [fullImage, setFullImage] = React.useState('');
   const [errorImage, setErrorImage] = React.useState();
@@ -41,26 +51,61 @@ function FullPin({route}) {
   }
 
   return (
-    <>
-      <SafeAreaView>
-        <StatusBar />
-        <ScrollView>
-          <View>
-            {fullImage.url && (
-              <Image source={{uri: fullImage.url}} style={styles.pin} />
-            )}
+    <View style={{flex: 1}}>
+      <View style={{flex: 2}}>
+        <TouchableOpacity
+          style={{flex: 1}}
+          onPress={() => setModalVisible(!modalVisible)}>
+          {fullImage.url && (
+            <Image source={{uri: fullImage.url}} style={styles.pin} />
+          )}
+        </TouchableOpacity>
+      </View>
+      <View style={styles.infoImage}>
+        <Text style={styles.titleImage}>{fullImage.title}</Text>
+      </View>
+      <Modal visible={modalVisible} transparent={true}>
+        <View style={{flex: 1}}>
+          <View
+            style={{width: '100%', height: '100%', backgroundColor: '#000'}}>
+            <TouchableOpacity
+              style={{marginTop: 15, marginLeft: 15}}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Image
+                source={require('../../public/img/close.png')}
+                style={{height: 40, width: 40}}
+              />
+            </TouchableOpacity>
+            <Pinchable>
+              <Image
+                source={{uri: fullImage.url}}
+                style={{height: '100%', width: '100%'}}
+                accessibilityLabel={'fullImage'}
+                resizeMode={'contain'}
+              />
+            </Pinchable>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+        </View>
+      </Modal>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   pin: {
+    flex: 1,
     width: null,
-    height: 400,
-    marginBottom: 10,
+    marginBottom: -20,
+  },
+  infoImage: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#ffffff',
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  titleImage: {
+    fontSize: 25,
   },
 });
 
